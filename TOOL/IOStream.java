ab7ee53d78ed4c5b2f5faa3844df6179e0fbd350
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-12-29 17:12:12 +0900

    Update IOStream.java

--- TOOL/IOStream.java
+++ TOOL/IOStream.java
@@ -1,33 +1,69 @@
 // import TOOL.*;
 // IOStream io = new IOStream();
-import java.util.Scanner;
 package TOOL;
-public class IOStrem
+import java.util.Scanner;
+public class util implements io
 {
-String ln="\n---------------------------------\n";
-public IOStream() {}
-public String scLine(String text)
-{
-Scanner sc = new Scanner(System.in);
-System.out.print(text+": ");
-return sc.nextLine();
+	public String ln="\n------------------------------------------------------\n";
+	@Override
+	public String scLine(String text)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print(text+": ");
+		return sc.nextLine();
+	}
+	@Override
+	public int scInt(String text)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print(text+": ");
+		return sc.nextInt();
+	}
+	@Override
+	public void printf(String text)
+	{
+		System.out.printf(text);
+	}
+	@Override
+	public void println(String text)
+	{
+		System.out.println(text);
+	}
+	@Override
+	public void print(String text)
+	{
+		System.out.print(text);
+	}
+	@Override
+	public void err(String text)
+	{
+		System.err.print(text);
+	}
+	@Override
+	public void errln(String text)
+	{
+		System.err.println(text);
+	}
+	@Override
+	public int toInt(String num)
+	{
+		return Integer.parseInt(num);
+	}
+	@Override
+	public void exit() 
+	{
+		System.exit(0);
+	}
 }
-public int scInt(String text)
+interface io
 {
-Scanner sc = new Scanner(System.in);
-System.out.print(text+": ");
-return sc.nextInt();
-}
-public printf(String text)
-{
-System.out.printf(text);
-}
-public println(String text)
-{
-System.out.println(text);
-}
-public print(String text)
-{
-System.out.print(text);
-}
+	public String scLine(String text);
+	int toInt(String num);
+	public int scInt(String text);
+	public void printf(String text);
+	public void println(String text);
+	public void print(String text);
+	public void err(String text);
+	public void errln(String text);
+	public void exit();
 }
