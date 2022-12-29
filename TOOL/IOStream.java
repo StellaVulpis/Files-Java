116a1a4b91d8170dbaba441d4b9654abb34e9c14
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-12-29 15:21:14 +0900

    Update IOStream.java

--- TOOL/IOStream.java
+++ TOOL/IOStream.java
@@ -1,1 +1,33 @@
 // import TOOL.*;
+// IOStream io = new IOStream();
+import java.util.Scanner;
+package TOOL;
+public class IOStrem
+{
+String ln="\n---------------------------------\n";
+public IOStream() {}
+public String scLine(String text)
+{
+Scanner sc = new Scanner(System.in);
+System.out.print(text+": ");
+return sc.nextLine();
+}
+public int scInt(String text)
+{
+Scanner sc = new Scanner(System.in);
+System.out.print(text+": ");
+return sc.nextInt();
+}
+public printf(String text)
+{
+System.out.printf(text);
+}
+public println(String text)
+{
+System.out.println(text);
+}
+public print(String text)
+{
+System.out.print(text);
+}
+}
